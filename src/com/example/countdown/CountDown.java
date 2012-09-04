94ab880995e3e6876c2704853a9f48720ffb9a1a
nagijun080 <jun0521.hikaru0531.sub.hakata@gmail.com>
2012-09-05 02:34:05 +0900

    first commit

---
+++ src/com/example/countdown/CountDown.java
@@ -0,0 +1,78 @@
+package com.example.countdown;
+
+
+import com.example.countdown.R;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RemoteViews;
+
+public class CountDown extends AppWidgetProvider {
+	
+	
+	private static final String ACTION_START_MY_ALARM =
+			"com.example.android.appwidget.countdown.ACTION_START_MY_ALARM";
+	private final long interval = 1000;
+	
+	@Override
+	public void onEnabled(Context context) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onEnabled(context);
+		setAlarm(context);
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onReceive(context, intent);
+		//Log.d("onReceive","superのあと");
+			if (ACTION_START_MY_ALARM.equals(intent.getAction())) {
+				Intent serviceIntent = new Intent(context, Myservice.class);
+				context.startService(serviceIntent);
+				//Log.d("onReceive","if文の中");
+			}
+			//Log.d("onReceiveの中","setAlarm()の前");
+			setAlarm(context);
+		}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO 自動生成されたメソッド・スタブ
+			Log.d("update","Updateの中");
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.activity_main);
+			
+			//呼び出したいActivityをセット
+			Intent intent = new Intent(context,MemorialDay.class);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+			
+			//widgetのボタンクリックイベントに呼び出したいIntentを設定する
+			remoteViews.setOnClickPendingIntent(R.id.getUrl, pendingIntent);
+			
+			ComponentName widget = new ComponentName(context,CountDown.class);
+			appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+			setAlarm(context);
+		}
+
+	private void setAlarm(Context context) {
+		Intent alarmIntent = new Intent(context, CountDown.class);
+		alarmIntent.setAction(ACTION_START_MY_ALARM);
+		PendingIntent operation = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+		AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+		long now = System.currentTimeMillis() + 1;// + 1は確実に未来時刻になるようにする
+		long oneSecondAfter = now + interval - now % (interval);
+		am.set(AlarmManager.RTC, oneSecondAfter, operation);
+	}
+	
+	
+
+}
---
+++ src/com/example/countdown/Myservice.java
@@ -0,0 +1,107 @@
+package com.example.countdown;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.example.countdown.R;
+
+
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+public class Myservice extends Service {
+  
+	private static String[] weekName = new String[] { "日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", };
+		
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO 自動生成されたメソッド・スタブ
+		RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.widget_main);
+		
+		Calendar calendar = Calendar.getInstance();
+		Calendar countDown = Calendar.getInstance();
+		
+		countDown.set(2012, Calendar.NOVEMBER, 1);
+		//今年の始めから１１月１日までの日数
+		Integer releaseDay = countDown.get(Calendar.DAY_OF_YEAR);
+		//今年の始めから現在までの日数
+		Integer nowDay = calendar.get(Calendar.DAY_OF_YEAR);
+		releaseDay -= nowDay; //現在の日にちから発売日までの日数をreleaseDayに代入
+		remoteViews.setTextViewText(R.id.AnniDay, releaseDay.toString());
+		
+		//年の初期化
+		Integer yearInt = calendar.get(Calendar.YEAR);
+		String year = new String(yearInt + "年");
+		//日付の初期化
+		Integer dateMonth = calendar.get(Calendar.MONTH) + 1;
+		Integer dateDay = calendar.get(Calendar.DATE);
+		String date = new String(dateMonth.toString() + "月" + dateDay.toString() + "日");
+		//曜日の初期化
+		Integer week = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+		//分の初期化
+		Integer minute = calendar.get(Calendar.MINUTE);
+		Integer minUD = minute % 10;
+		Integer minTD = minute / 10;
+		
+		String minUDSt = minUD.toString();
+		String minTDSt = minTD.toString();
+		//時間の初期化
+		Integer hour,hrUD,hrTD;
+		
+		if (Calendar.AM == calendar.get(Calendar.AM_PM)) {
+			hour = calendar.get(Calendar.HOUR);
+			hrUD = hour % 10;
+			hrTD = hour / 10;
+		} else {
+			hour = calendar.get(Calendar.HOUR) + 12;
+			hrUD = hour % 10;
+			hrTD = hour / 10;
+		}
+		String hrUDSt = hrUD.toString();
+		String hrTDSt = hrTD.toString();
+		//年をR.id.yearに表示
+		remoteViews.setTextViewText(R.id.year, year);
+		//日付をR.id.dayに表示
+		remoteViews.setTextViewText(R.id.day, date);
+		//曜日をwidget_main.xmlのweekに表示
+		remoteViews.setTextViewText(R.id.week, weekName[week]);
+		
+		//十の位の時数をhrTenthsDigitに表示
+		remoteViews.setTextViewText(R.id.hrTenthsDigit, hrTDSt);
+		//一の位の時数をhrUnitDigitに表示
+		remoteViews.setTextViewText(R.id.hrUnitDigit, hrUDSt);
+				
+		
+		//十の位の分数をminTenthsDigitに表示
+		remoteViews.setTextViewText(R.id.minTenthsDigit, minTDSt);
+		//一の位の分数をminUnitDigitに表示
+		remoteViews.setTextViewText(R.id.minUnitDigit, minUDSt);
+		
+		remoteViews.setTextViewText(R.id.textAnniversary, getText(R.string.release_first));
+		
+		
+		ComponentName thisWidget = new ComponentName(this, CountDown.class);
+		AppWidgetManager manager = AppWidgetManager.getInstance(this);
+		manager.updateAppWidget(thisWidget, remoteViews);
+	}
+
+	
+
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+	// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	
+}
