f79e9e4f7b498f6e399d5a21938f91f145c2bb42
nagijun080 <jun0521.hikaru0531.sub.hakata@gmail.com>
2012-10-15 05:57:17 +0900

    コメント用

--- src/com/example/countdown/MemorialDayActivity.java
+++ src/com/example/countdown/MemorialDayActivity.java
@@ -218,5 +218,4 @@ public class MemorialDayActivity extends Activity{
 	 private LinearLayout.LayoutParams createParam(int w, int h){
 	        return new LinearLayout.LayoutParams(w, h);
 	 }
-	
 }
